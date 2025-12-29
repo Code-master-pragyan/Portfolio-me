fd9ecc7b50f03d497c22acfcf8ed789162be2854
Pragyan Lahkar <pragyanlahkar28@gmail.com>
2025-12-29 21:08:30 +0530

    Remove env file and clean history

--- app.js
+++ app.js
@@ -12,16 +12,30 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+
+const allowedOrigins = [
+    "https://myselfpragyan.onrender.com",
+    "https://pragyan.xyz"
+];
+
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://myselfpragyan.onrender.com");
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+    }
+
     res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Credentials", "true");
 
     if (req.method === "OPTIONS") {
         return res.sendStatus(200);
     }
+
     next();
 });
+
 app.use(express.json());
 app.engine('ejs', ejsMate);
 
